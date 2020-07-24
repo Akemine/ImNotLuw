2c5d3d79421fdb14b619055b8209c93222a19837
Stephane <stephane.mestric@protonmail.com>
2020-07-25 00:29:23 +0200

    Revert "Redux non utilisé"
    This reverts commit b3fe2703

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import * as reducers from './reducers/reducers';
-import { combineReducers } from 'redux';
-import { createStore } from 'redux';
-
-const galerieAppReducers = combineReducers(reducers)
-const store = createStore(galerieAppReducers)
-
-export default store
